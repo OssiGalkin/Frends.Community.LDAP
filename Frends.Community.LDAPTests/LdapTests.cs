6627bdc7fc432acc52e61a0bf53930a955d1ef2c
Ossi Galkin <ossi.galkin@hiq.fi>
2019-09-18 12:54:51 +0300

    Marked test project with <IsTestProject>

--- Frends.Community.LDAPTests/LdapTests.cs
+++ Frends.Community.LDAPTests/LdapTests.cs
@@ -1,7 +1,5 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
 using Frends.Community.LDAP.Models;
 using Frends.Community.LDAP;
 using TestConfigurationHandler;
